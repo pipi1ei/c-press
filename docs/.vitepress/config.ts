783af66837b1ec556141bf956307e7103ca197d5
pipi1ei <805396880@qq.com>
2026-01-19 10:58:30 +0800

    chore: config unocss

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitepress';
-import { getSidebarConfig } from './utils';
+import UnoCSS from 'unocss/vite';
 
 const BASE_DIR = '/c-press/';
 export default defineConfig({
@@ -7,13 +7,14 @@ export default defineConfig({
   description: '由VitePress驱动的个人博客站点',
   base: BASE_DIR,
   head: [['link', { rel: 'icon', href: `${BASE_DIR}favicon.svg` }]],
+  vite: {
+    plugins: [UnoCSS({})],
+  },
   themeConfig: {
     logo: '/vitepress-logo.svg',
-
     search: {
       provider: 'local',
     },
-
     nav: [
       { text: 'Home', link: '/' },
       {
@@ -26,32 +27,36 @@ export default defineConfig({
       },
       { text: 'AI', link: '/ai/basic/prompt', activeMatch: '/ai/' },
     ],
-
     sidebar: {
       '/frontend/': [
         {
           text: 'JavaScript',
-          items: [{ text: '竞态问题', link: '/frontend/js/race-condition' }],
+          items: [{ text: '竞态问题', link: '/frontend/js/race-condition/index.md' }],
         },
         {
           text: 'Vue',
-          items: [{ text: '响应式原理', link: '/frontend/vue/reactivity' }],
+          items: [{ text: '响应式原理', link: '/frontend/vue/reactivity/index.md' }],
         },
       ],
       '/ai/': [{ text: '提示词工程', link: '/ai/basic/prompt' }],
     },
-
     socialLinks: [{ icon: 'github', link: 'https://github.com/pipi1ei/c-press' }],
-
+    outline: {
+      label: '----In this page----',
+    },
     docFooter: {
       prev: '上一页',
       next: '下一页',
     },
-
     footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright ©2025-present chenlei',
     },
   },
-  lastUpdated: true,
+  markdown: {
+    theme: {
+      light: 'one-light',
+      dark: 'one-dark-pro',
+    },
+  },
 });
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import Layout from './Layout.vue';
 import './styles.css';
+import 'virtual:uno.css';
 
 export default {
   extends: DefaultTheme,
